787869690ad6fcc2479f539be241709d50a6fc4f
fangdc <fang_gco@163.com>
2020-09-15 20:36:02 +0800

    Initial Commit

---
+++ Shared/HolerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolerApp.swift
+//  Shared
+//
+//  Created by 方大葱的MacBook Pro on 2020/9/15.
+//
+
+import SwiftUI
+
+@main
+struct HolerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
